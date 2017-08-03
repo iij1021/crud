a0fa64003f3f6933c9bb39233006b64338ec0d4d
iij <iij1021@outlook.com>
2017-08-03 14:20:30 -0300

    Login

--- WCrud/WCrud/Form1.cs
+++ WCrud/WCrud/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace WCrud
 {
-    public partial class Form1 : Form
+    public partial class FLogin : Form
     {
-        public Form1()
+        private System.Windows.Forms.DataGrid mDataGrid;
+        private MySqlConnection Conexao;
+        private MySqlDataAdapter mAdapter;
+        private DataSet mDataSet;
+        private MySqlDataReader reader   ;
+                                
+        public FLogin()
         {
             InitializeComponent();
+            mDataSet = new DataSet();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string configuracao = "server = localhost;uid = root;database = empresa";
+            Conexao = new MySqlConnection(configuracao);
+            try
+            {
+                Conexao.Open();
+               
+                if (Conexao.State == ConnectionState.Open)
+                {
+                   MySqlCommand sql = new MySqlCommand("SELECT * FROM CLIENTE WHERE idcliente =@id",Conexao);
+                   MySqlParameter parametros = new MySqlParameter();
+                    parametros.ParameterName = "@id";
+                    sql.Parameters.Add(parametros);
+                    reader = sql.ExecuteReader();
+
+                    MessageBox.Show("Clientes " + reader.);
+
+                }
+
+            }
+            catch(System.Exception E)
+            {
+                MessageBox.Show(" Erro " + E.Message);
+            }
         }
     }
 }
